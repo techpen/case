ce35e5b601bb354789c66a0b86afece41c4654d2
techpen <codingpen@gmail.com>
2017-03-06 16:41:30 +0800

    datayin
    add case

---
+++ datayin/js/goTop.js
@@ -0,0 +1,28 @@
+/**
+ * Created by weigang on 2016/4/7.
+ */
+function goTopEx() {
+    var obj = document.getElementById("goTop");
+    function getScrollTop() {
+        return document.documentElement.scrollTop + document.body.scrollTop;
+    }
+    function setScrollTop(value) {
+        if (document.documentElement.scrollTop) {
+            document.documentElement.scrollTop = value;
+        } else {
+            document.body.scrollTop = value;
+        }
+    }
+    window.onscroll = function() {
+        getScrollTop() > 0 ? obj.style.display = "" : obj.style.display = "none";
+    }
+    obj.onclick = function() {
+        var goTop = setInterval(scrollMove, 10);
+
+        function scrollMove() {
+            setScrollTop(getScrollTop() / 1.1);
+            if (getScrollTop() < 1) clearInterval(goTop);
+        }
+    }
+}
+
